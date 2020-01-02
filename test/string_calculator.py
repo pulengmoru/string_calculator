7b15f86d7fcd1306a5f0c906cda26cb25b276a00
pulengmoru <54172752+pulengmoru@users.noreply.github.com>
2020-01-02 22:03:00 +0200

    Add files via upload

---
+++ test/string_calculator.py
@@ -0,0 +1,23 @@
+import re
+import numpy as np
+
+def add(value_list):
+
+	value_list = []
+	
+	if value_list = array([""])
+	return == 0
+
+	elif value_list = array(["1"])
+	return == 0
+
+	elif value_list = array(["1,2"])
+	return == 2
+
+	else:
+		value_list = negatives.append()
+if negatives:
+	raise ValueError(f'negatives not alloed. found {negatives}')
+
+	len(value_list)
+print(value_list)
---
+++ test/test_calculator.py
@@ -0,0 +1,35 @@
+from string_calculator import add
+import numpy as np
+
+def test_return_0_for_empty_string():
+	result = add("") 
+	assert result == 0
+
+def test_single_digit_strings():
+	result = add("1")
+	assert result == 1
+
+	result = add("22")
+	assert result == 22
+
+def test_string_with_two_or_more_elements_return_sum():
+	result = add("1, 2")
+	assert result == 3
+
+	result = add("2, 3")
+	assert result == 5
+
+	result = add("1,2,3,4")
+	assert result == 10
+
+def test_newline_delimiter():
+	result = add("1\n2,3")
+	assert result == 6
+
+def test_different_delimiters():
+	result = add("//;\n1;2")
+	assert result == 3
+
+def test_negative_numbers_raises_exception():
+	result = add("-1")
+	assert NegativeError

a86efd898cf4b28a7fd722860a5a1ef454fe9dbe
pulengmoru <54172752+pulengmoru@users.noreply.github.com>
2020-01-17 12:31:54 +0200

    Second commit

--- test/string_calculator.py
+++ test/string_calculator.py
@@ -1,3 +1,4 @@
+import re
 def add(num_string):
 	if len(num_string) == 0:
 		return 0
@@ -17,4 +18,35 @@ def add(num_string):
 	else:
 		nums = num_string.split(",")
 		return sum(int(nums))
-print(add("22"))
+print(add("2,2"))
+
+def add(s):
+	if len(s) == 0:
+		return 0
+	reg = re.findall(r'\d+|-\d+' , s)
+	total = 0
+
+	# exctract negative numbers from reg list
+
+	negs = getAllNegatives(reg)
+	if len(negs) > 0:
+		error = 'ERROR: negatives not allowed '
+		for i in range(len(negs)):
+			if i == len(negs) - 1:
+				error = error + negs[i]
+			else:
+				error = error + negs[i] + ','
+		raise Exception(error)
+
+	for number in reg:
+		if int(number) < 1000:
+			total = total + int(number)
+	return total
+def getAllNegatives(nums):
+	negs = []
+	for num in nums:
+		if int(num) < 0:
+			negs.append(num)
+	return negs 
+total_sum  = add("//;\n1000,1;2")
+print(total_sum)
--- test/test_calculator.py
+++ test/test_calculator.py
@@ -9,6 +9,16 @@ def test_single_digit_strings():
 	result = add("1")
 	assert result == 1
 
+	result = add("1,2,3,4")
+	assert result == 10
+
+
+	result = add("2, 3")
+	assert result == 5
+
+	result = add("1,2,3,4")
+	assert result == 10
+
 	result = add("2000")
 	assert result == 2000
 
@@ -16,24 +26,17 @@ def test_string_with_two_or_more_elements():
 	result = add("1, 2")
 	assert result == 3
 
-	result = add("1,2,3,4")
-	assert result == 10
+def test_newline_delimiters():
+	result = add("1\n2,3")
+	assert result == 6
+
+def test_different_delimiters():
+	result = add("//;\n1;2")
+	assert result == 3
+
+def test_dnegative_numbers_raises_exception():
+	result = add("-1")
+	assert NegativeError
 
 
-# 	result = add("2, 3")
-# 	assert result == 5
-
-# 	result = add("1,2,3,4")
-# 	assert result == 10
-
-# def test_newline_delimiter():
-# 	result = add("1\n2,3")
-# 	assert result == 6
-
-# def test_different_delimiters():
-# 	result = add("//;\n1;2")
-# 	assert result == 3
-
-# def test_ dnegative_numbers_raises_exception():
-# 	result = add("-1")
-# 	assert NegativeError
+	
